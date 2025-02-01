60ee5380e5ceb902cdfb3710a589c03e7e58c1bc
Elijah Quibin <epquibin@student.apc.edu.ph>
2025-02-01 20:15:11 +0800

    ADDED APPVUE

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,11 @@
 <script></script>
 
 <template>
-  <h1>Hello WEBPROG SS231 world</h1>
+  <h1>Food</h1> 
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
 </template>
 
 <style></style>
