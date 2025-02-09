dc4c328da575c8359f34de442654a95a363375b6
Elijah Quibin <epquibin@student.apc.edu.ph>
2025-02-09 22:30:25 +0800

    ss

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,35 +1,16 @@
-<script>
-import { createClient } from '@supabase/supabase-js';
-import CommentForm from './components/CommentForm.vue';
-
-const supabaseUrl = 'https://your-supabase-url.supabase.co';
-const supabaseKey = 'your-supabase-key';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default {
-  components: {
-    CommentForm,
-  },
-  methods: {
-    async addComment(name, comment) {
-      const { data, error } = await supabase
-        .from('comments')
-        .insert([{ name, comment }]);
-      if (error) console.error(error);
-      else console.log('Comment added:', data);
-    }
-  }
-};
-</script>
-
 <template>
   <h1>Food</h1>
-  <instrument/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <comment-form/>
-  <comment/>
+  <instruments/>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <comment-form/> <br>
+  <comment/> <br>
+  <personal-profile/> <br>
 </template>
 
-<style></style>
+
+<style> </style>
+
+<script> </script>
