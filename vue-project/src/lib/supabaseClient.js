271bbfea0751d812beaed222622666a79003d9cb
Elijah Quibin <epquibin@student.apc.edu.ph>
2025-02-09 20:53:11 +0800

    del supabse in main

--- vue-project/src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://hmtzrdosowbjnzgnydfk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtdHpyZG9zb3diam56Z255ZGZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0MjExNTcsImV4cCI6MjA1Mzk5NzE1N30.JgmPD6qP9dyLVWjqYtOw7TWY2Amw44h1knzzFMwf_D0')
